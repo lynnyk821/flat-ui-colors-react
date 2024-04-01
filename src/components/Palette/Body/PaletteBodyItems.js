544343f58ab47454f3c09b235a79fde0cc1e00c2
Yaroslav <lynnyk265@gmail.com>
2024-04-01 21:25:50 +0300

    add function to copy to clipboard

--- src/components/Palette/Body/PaletteBodyItems.js
+++ src/components/Palette/Body/PaletteBodyItems.js
@@ -1,10 +1,21 @@
 export const PaletteBodyItems = ({ colors }) => {
+    const copyToClipboard = (color) => {
+        navigator.clipboard.writeText(color)
+            .then(() => {
+                alert(`Color copied to clipboard: ${color}`);
+            })
+            .catch((error) => {
+                console.error('Error copying olor to clipboard:', error);
+            });
+    };
     return (
         <div className="w-full h-full grid grid-cols-5">
-            {colors.map(element => (
+            {colors.map((element, id) => (
                 <button
                     className="w-full h-full flex justify-end items-end flex-col p-3"
                     style={{ background: `${element.color}` }}
+                    id={`palette-color-${id}`}
+                    onClick={() => copyToClipboard(element.color)}
                 >
                 <p className="text-lg text-white font-medium">{element.name.toUpperCase()}</p>
               </button>
